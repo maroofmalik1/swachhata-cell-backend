c609f5a31c9472804563a5db542a6ada12ff143a
Mohd Maroof <146874434+maroofmalik1@users.noreply.github.com>
2025-02-07 12:40:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // Middleware
-app.use(cors({ origin: "https://swachhata-cell-frontend.vercel.app" }));
+app.use(cors({ origin: ["https://swachhata-cell-frontend.vercel.app"], methods: ["GET", "POST"], credentials: true }));
 app.use(express.json());
 
 // Database Connection
