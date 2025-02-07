1026e34491d668223395d109b43a3f6cd9ab3605
Mayank Jha <mayank.jha@only4values.com>
2025-02-07 12:24:34 +0530

    added frontend url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "https://swachhata-cell-frontend.vercel.app" }));
 app.use(express.json());
 
 // Database Connection
